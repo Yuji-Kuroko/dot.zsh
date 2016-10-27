9b8c444e2a06c8aed1c7e84056ea9407b189b627
yuji.kuroko <yuji.kuroko@jp.oro.com>
2016-10-27 12:46:06 +0900

    increase HISTSIZE

--- conf.d/history.sh
+++ conf.d/history.sh
@@ -1,6 +1,6 @@
 HISTFILE=~/.zsh_history
-HISTSIZE=10000
-SAVEHIST=10000
+HISTSIZE=100000
+SAVEHIST=100000
 setopt hist_ignore_dups   # ignore duplication command history list
 setopt hist_ignore_space  # half space
 setopt share_history    # share command history data
