06d5df68501c0f148c3e8586014496f09408c8dc
yuji.kuroko <yuji.kuroko@jp.oro.com>
2017-01-24 19:08:52 +0900

    fix lunchy error when opened tmux

--- platforms/Darwin/conf.d/lunchy.sh
+++ platforms/Darwin/conf.d/lunchy.sh
@@ -1,6 +1,8 @@
 # for gem lunchy
 
-LUNCHY_DIR=$(dirname `gem which lunchy`)/../extras
-if [ -f $LUNCHY_DIR/lunchy-completion.zsh ]; then
-  . $LUNCHY_DIR/lunchy-completion.zsh
+if which lunchy > /dev/null 2>&1; then
+  LUNCHY_DIR=$(dirname `gem which lunchy`)/../extras
+  if [ -f $LUNCHY_DIR/lunchy-completion.zsh ]; then
+    . $LUNCHY_DIR/lunchy-completion.zsh
+  fi
 fi
