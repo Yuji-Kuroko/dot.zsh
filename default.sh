1d6cca59ffacd42415db1e52cac384d52b10c52d
yuji.kuroko <yuji.kuroko@jp.oro.com>
2025-05-24 16:51:58 +0900

    fix: TAB保管時に色がおかしくならないようにする

--- default.sh
+++ default.sh
@@ -12,31 +12,31 @@ setopt prompt_subst
 APPLE_SILICON_DISPLAY=""
 if test $(uname) = "Darwin" && arch -arm64e echo ok &>/dev/null; then
   if [ "$(uname -m)" = "x86_64" ]; then
-    APPLE_SILICON_DISPLAY=" %F{magenta}x86_64%f%F{cyan}"
+    APPLE_SILICON_DISPLAY=" %F{magenta}x86_64%f%F{cyan}%f"
   elif [ "$(uname -m)" = "arm64" ]; then
-    APPLE_SILICON_DISPLAY=" %F{white}arm64%f%F{cyan}"
+    APPLE_SILICON_DISPLAY=" %F{white}arm64%f%F{cyan}%f"
   else
-    APPLE_SILICON_DISPLAY=" %F{white}UNKNOWN%f%F{cyan}"
+    APPLE_SILICON_DISPLAY=" %F{white}UNKNOWN%f%F{cyan}%f"
   fi
 fi
 
-PROMPT="%B%{[36m%}[%n@%m${APPLE_SILICON_DISPLAY}]#%{[m%}%b "
+PROMPT="%B%{$fg[cyan]%}[%n@%m${APPLE_SILICON_DISPLAY}]#%{$reset_color%}%b "
 
 case ${UID} in
 0)
   [[ -n "$vcs_info_msg_0_" ]] && psvar[1]="$vcs_info_msg_0_"
-  PROMPT="%B%{[36m%}[%n@%m${APPLE_SILICON_DISPLAY}]#%{[m%}%b "
-  PROMPT2="%B%{[31m%}%_#%{[m%}%b "
-  RPROMPT="%{[36m%}(`rprompt_git_current_branch`%{[36m%}%~%)%{[m%}"
-  SPROMPT="%B%{[31m%}%r is correct? [n,y,a,e]:%{${reset_color}%}%b "
+  PROMPT="%B%{$fg[cyan]%}[%n@%m${APPLE_SILICON_DISPLAY}]#%{$reset_color%}%b "
+  PROMPT2="%B%{$fg[red]%}%_#%{$reset_color%}%b "
+  RPROMPT="%{$fg[cyan]%}(`rprompt_git_current_branch`%{$fg[cyan]%}%~%)%{$reset_color%}"
+  SPROMPT="%B%{$fg[red]%}%r is correct? [n,y,a,e]:%{$reset_color%}%b "
   [ -n "${REMOTEHOST}${SSH_CONNECTION}" ] &&
   PROMPT="%{${fg[white]}%}${HOST%%.*} ${PROMPT}"
   ;;
 *)
-  PROMPT="%{[36m%}[%n@%m${APPLE_SILICON_DISPLAY}]%%%{[m%} "
-  PROMPT2="%{[31m%}%_%%%{[m%} "
-  RPROMPT='%{[36m%}(`rprompt_git_current_branch`%{[36m%}%~%)%{[m%}'
-  SPROMPT="%{[31m%}%r is correct? [n,y,a,e]:%{${reset_color}%} "
+  PROMPT="%{$fg[cyan]%}[%n@%m${APPLE_SILICON_DISPLAY}]%%%{$reset_color%} "
+  PROMPT2="%{$fg[red]%}%_%%%{$reset_color%} "
+  RPROMPT='%{$fg[cyan]%}(`rprompt_git_current_branch`%{$fg[cyan]%}%~%)%{$reset_color%}'
+  SPROMPT="%{$fg[red]%}%r is correct? [n,y,a,e]:%{$reset_color%} "
   [ -n "${REMOTEHOST}${SSH_CONNECTION}" ] &&
   PROMPT="%{${fg[white]}%}${HOST%%.*} ${PROMPT}"
   ;;
